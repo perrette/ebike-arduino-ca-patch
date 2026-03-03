9e022f573af90db0d1639f6fd93ac26be3e77f2c
perrette <mahe.perrette@gmail.com>
2026-03-03 15:55:16 +0100

    Refactor input handling and state management for improved clarity and functionality

--- main.cpp
+++ main.cpp
@@ -18,70 +18,108 @@
 #define TORQUE_SCALE 100 // Nm / V
 
 // PAS CONFIG SETTINGS (voir Cycle Analyst PAS Config Settings : https://www.cycleanalyst.com/pas-config-settings/)
-#define SPEED_START_THRES = 0.3 // sec / pole
-#define SPEED_STOP_THRES = 0.19 // sec / pole
+#define SPEED_START_THRES = 0.3      // sec / pole
+#define SPEED_STOP_THRES = 0.19      // sec / pole
 #define TORQUE_ASSIST_AVERAGING = 48 // poles
+
+// Phaserunner settings
+#define THROTTLE_OUT_NEUTRAL_VOLTAGE 0.8 // V
+
 #define Throttle_Value TCA0.SINGLE.CMP2 // Raw PWM Throttle Value Range : [0-1023]
 
-uint16_t counter;
-uint16_t last_speed_state;
+// LOOP OPTIMIZATION SETTINGS (unused)
+#define Throttle_Value TCA0.SINGLE.CMP2 // Raw PWM Throttle Value Range : [0-1023]
+
+// uint16_t counter;
+// uint16_t last_speed_state;
 bool moving_forward;
-unsigned long last_pole_time;
-unsigned long current_speed;
-unsigned long avg_speed;
+// unsigned long last_pole_time;
+// unsigned long current_speed;
+// unsigned long avg_speed;
 
-void init_counter() {
-  counter = 0;
-  last_speed_state = LOW;
-  last_pole_time = millis();
-  moving_forward = true;
+// unsigned long speeds[TORQUE_ASSIST_AVERAGING];
+uint64_t POLES_SINCE_START_AVG = 0;
+uint64_t TIME_SINCE_START_AVG = 0;
+uint64_t START_AVG_TIME = 0;
+
+// Global state variables
+int direction;
+int speed;
+float torque;
+float throttle_out;
+
+void update_inputs()
+{
+  direction = digitalRead(PIN_DIRECTION_IN);
+  speed = digitalRead(PIN_SPEED_IN);
+  torque = analogRead(PIN_TORQUE_IN) * ANALOG_TO_VOLT_IN;
 }
 
-void start_moving() {
-  counter = 0;
-  last_speed_state = LOW;
-  last_pole_time = millis();
-  moving_forward = true;
-}
-
-void stop_moving() {
+void init_counter()
+{
   // counter = 0;
   // last_speed_state = LOW;
   // last_pole_time = millis();
   moving_forward = false;
 }
 
-void check_direction() {
-  if (digitalRead(PIN_DIRECTION_IN) == HIGH) {
-    if (!moving_forward) {
-      start_moving();
-    }
-  } else {
-    if (moving_forward) {
-      start_moving();
-    }
+void start_moving()
+{
+  // counter = 0;
+  // last_speed_state = LOW;
+  // last_pole_time = millis();
+  POLES_SINCE_START_AVG = 0;
+  TIME_SINCE_START_AVG = 0;
+  START_AVG_TIME = millis();
+  moving_forward = true;
+}
+
+void stop_moving()
+{
+  moving_forward = false;
+  throttle_out = THROTTLE_OUT_NEUTRAL_VOLTAGE;
+}
+
+void check_direction()
+{
+
+  direction = digitalRead(PIN_DIRECTION_IN);
+
+  if (direction == LOW)
+  {
+    stop_moving();
   }
 }
 
-void check_speed() {
+void check_speed()
+{
+
+  check_direction(); // we check the direction to update the moving_forward state
+
+  if (direction == HIGH && !moving_forward)
+  {
+    start_moving();
+  }
+
+  speed = digitalRead(PIN_SPEED_IN);
   // only update speed and counter if we are moving forward, otherwise we are not interested in the speed and counter values
-  if (!moving_forward) {
+  if (!moving_forward)
+  {
     return;
   }
   // we are moving forward
 
-
-  counter++ ;
-  unsigned long current_time = millis();
-  if (last_speed_state == LOW && digitalRead(PIN_SPEED_IN) == HIGH) {
-    current_speed = 1000. / (current_time - last_pole_time); // Speed in poles per second
-    last_pole_time = current_time;
-  }
-  last_speed_state = digitalRead(PIN_SPEED_IN);
+  // // counter++ ;
+  // unsigned long current_time = millis();
+  // if (last_speed_state == LOW && digitalRead(PIN_SPEED_IN) == HIGH) {
+  //   current_speed = 1000. / (current_time - last_pole_time); // Speed in poles per second
+  //   last_pole_time = current_time;
+  // }
+  // last_speed_state = digitalRead(PIN_SPEED_IN);
 }
 
-
-void setup() {
+void setup()
+{
   pinMode(PIN_DIRECTION_IN, INPUT);
   pinMode(PIN_SPEED_IN, INPUT);
   pinMode(PIN_TORQUE_IN, INPUT);
@@ -100,11 +138,9 @@ void setup() {
   // TCA0.SINGLE.CTRLA = 1;         // Enable TimerA
 }
 
-
-void loop() {
-  float torque = analogRead(PIN_TORQUE_IN) * ANALOG_TO_VOLT_IN;
-  int speed = digitalRead(PIN_SPEED_IN);
-  int direction = digitalRead(PIN_DIRECTION_IN);
+void loop()
+{
+  update_inputs();
   float torque_cursor = (torque - BASE_TORQUE) / (MAX_TORQUE - BASE_TORQUE);
   // float throttle_out = torque_cursor * (torque_cursor > 0 ? 1 : 0) * (direction == HIGH ? 1 : 0) * (speed == HIGH ? 1 : 0);
   float throttle_out = torque;
