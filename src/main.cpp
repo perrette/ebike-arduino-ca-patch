55b77c20845cec7f6e418a533cc6a0a722292575
perrette <mahe.perrette@gmail.com>
2026-03-05 09:24:52 +0100

    add the direction pin: in case a Falling event is detected, stop the bike at the next loop iteration

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,13 @@
 void SpeedPulseEvent(); // Interrupt routine called each rising edge on Speed
                         // Signal
 
+void StopBikeEvent(); // Interrupt routine called on falling edge of DirectionPin
+void ResetBike(); // Function to reset the bike, can be called in case of emergency or to restart the bike
+void ResetTime(); // Function to reset the loop time, to be called when stopping the bike to avoid a long wait before the next loop
+void StopBike();  // Function to stop the bike by resetting the state and applying the neutral
+
 // I/O configuration
+#define DirectionPin 3      // Direction Pin on D4
 #define SpeedPin 4          // Speed Pin on D4
 #define ThrottlePin 5       // Throttle Pin on D5
 #define ThrottleCheckPin A1 // Throttle Pin on A1
@@ -28,8 +34,8 @@ uint8_t RpmStopDetector; // +1 each loop, and cleared each speed pulse. If this 
 #define BikerMaxRpm 200      // Max biker rpm to map the throttle
 float BikerRpm;              // Raw Biker RPM value
 float BikerRpmFiltered;      // Filtered Biker RPM value
-uint32_t TimeBetween2edge;   // Time between 2 rising edges on Speed Signal (uSec)
-uint32_t LastTimeOfLastEdge; // Time of last edge on speed signal (uSec)
+volatile uint32_t TimeBetween2edge;   // Time between 2 rising edges on Speed Signal (uSec)
+volatile uint32_t LastTimeOfLastEdge; // Time of last edge on speed signal (uSec)
 
 // from sensor data sheet
 #define TorqueValueNeutral 1.5f
@@ -42,23 +48,28 @@ uint32_t LastTimeOfLastEdge; // Time of last edge on speed signal (uSec)
 // #define TorqueValueMax 500.0f // Maximum torque value in Nm, to be calibrated
 #define TorqueValueFilteredAlphaGain 0.9f
 float TorqueValue;
-float TorqueValueFiltered = TorqueValueNeutral; // initialize filtered torque value to neutral value to avoid a jump at the beginning
+float TorqueValueFiltered ; // Set in ResetBike() to neutral value to avoid a jump at the beginning
 
 #define HumanPowerWattMax 1000.0f // Constrain the calculation to that value
 #define HumanPowerWattAlphaGain 0.0f
 #define MotorPowerBoostFactor 1.0f // Motor Watt per Human Watt : Gain to convert the human power to motor power
 float HumanPowerWatt;
-float HumanPowerWattFiltered = 0.0f;
+float HumanPowerWattFiltered; // set in ResetBike() to zero to avoid a jump at the beginning
 
 // Throttle :
-float Throttle_Value_Volt = 0.8;
+#define MinThrottleValue 1.1f
+#define MaxThrottleValue 3.8f
+#define NeutralThrottleValue 1.0f
+#define MotorPowerAtMaxThrottle 1500.0f // Motor power in Watt at max throttle
+
+float Throttle_Value_Volt; // set in ResetBike() to zero to avoid a jump at the beginning
 uint16_t Throttle_Value =
     Throttle_Value_Volt *
     VOLT_TO_ANALOG_OUT; // Throttle Value : [0-1023] = Duty Cycle [0-100]%
 // 128;     // Throttle Value : [0-1023] = Duty Cycle [0-100]%
-#define MinThrottleValue 1.1f
-#define MaxThrottleValue 3.5f
-#define MotorPowerAtMaxThrottle 1500.0f // Motor power in Watt at max throttle
+
+// State machine
+volatile bool StopMotorPower = false; // Set to true in the interrupt routine to stop the bike
 
 // Timin
 #define LoopTimeUs 50000UL // Main loop duration (uSec)
@@ -67,6 +78,7 @@ uint32_t NextLoopTime;     // For loop time calibration
 void setup()
 {
   // I/O init
+  pinMode(DirectionPin, INPUT_PULLUP);
   pinMode(SpeedPin, INPUT_PULLUP);
   pinMode(ThrottlePin, OUTPUT);
   pinMode(ThrottleCheckPin, INPUT);
@@ -75,8 +87,12 @@ void setup()
   Serial.begin(115200);
 
   // Interrupt init
+  attachInterrupt(digitalPinToInterrupt(DirectionPin), StopBikeEvent,
+                  FALLING); // Call StopBikeEvent each falling edge on DirectionPin
   attachInterrupt(digitalPinToInterrupt(SpeedPin), SpeedPulseEvent,
                   RISING); // Call SpeedPulseEvent each rising edge on SpeedPin
+
+  ResetBike(); // Reset the bike to be sure to start with a known state
 }
 
 #define DEBUG_MODE true
@@ -101,12 +117,18 @@ void loop()
   if (RpmStopDetector++ > (1000 * TimeStopLimit / LoopTimeUs)) // Reset Biker Rpm if the time since the last pulse exceeds TimeStopLimit
     BikerRpmFiltered = 0;
 
+  if (StopMotorPower) // If StopMotorPower is set to true in the interrupt routine, stop the bike
+  {
+    StopBike() ; // Stop the bike by resetting the state and applying the neutral throttle value
+    return; // Skip the rest of the loop
+  }
+
   BikerRpmFiltered = constrain(BikerRpmFiltered, 0, BikerMaxRpm);
 
   if (DEBUG_MODE)
     BikerRpmFiltered = 30.0f; // Remove @ Release
 
-      TorqueValue = analogRead(TorquePin) * ANALOG_TO_VOLT_IN;
+  TorqueValue = analogRead(TorquePin) * ANALOG_TO_VOLT_IN;
   TorqueValueFiltered = TorqueValueFiltered * TorqueValueFilteredAlphaGain + TorqueValue * (1 - TorqueValueFilteredAlphaGain);
   TorqueValueFiltered = constrain(TorqueValueFiltered, TorqueValueNeutral, TorqueValueMax);       // Constrain filtered torque value to be between 0 and reference voltage
   float TorqueValueFilteredNm = (TorqueValueFiltered - TorqueValueNeutral) * TorqueSensorGain; // Convert voltage to torque in Nm, and remove the neutral value
@@ -130,7 +152,7 @@ void loop()
   if (DEBUG_MODE)
   {
     Serial.print(">"); // Remove @ Release
-    Serial.print("Raw_time_between_2_edge_uS:");
+    Serial.print("RPM:");
     Serial.print(BikerRpm);
     Serial.print(",Filtered_RPM:");
     Serial.print(BikerRpmFiltered);
@@ -138,12 +160,14 @@ void loop()
     Serial.print(TorqueValue);
     Serial.print(",Filtered_Torque_V:");
     Serial.print(TorqueValueFiltered);
-    Serial.print(",Filtered_Torque_Nm:");
+    Serial.print(",Torque_Nm:");
     Serial.print(TorqueValueFilteredNm);
     Serial.print(",Human_Power_Watt:");
     Serial.print(HumanPowerWatt);
     Serial.print(",Filtered_Human_Power_Watt:");
     Serial.print(HumanPowerWattFiltered);
+    Serial.print(",Motor_Power_Watt:");
+    Serial.print(MotorPower);
     Serial.print(",Throttle_Value:");
     Serial.print(Throttle_Value_Volt);
     Serial.print(",Throttle_Check:");
@@ -165,4 +189,29 @@ void SpeedPulseEvent() // Interrupt called each rising edge and refresh time
     TimeBetween2edge =
         CurrentTime - LastTimeOfLastEdge; // Get time since last edge
   LastTimeOfLastEdge = CurrentTime;       // Save current time for next calculation
+}
+
+void StopBikeEvent() // Interrupt called on falling edge of DirectionPin
+{
+  StopMotorPower = true; // Set flag to stop the bike
+}
+
+void ResetBike() // Function to reset the bike, can be called in case of emergency or to restart the bike
+{
+  StopMotorPower = false; // Reset StopMotorPower flag
+  BikerRpmFiltered = 0;   // Set filtered RPM to zero
+  TorqueValueFiltered = TorqueValueNeutral; // Set torque to neutral value to avoid a jump when restarting
+  HumanPowerWattFiltered = 0; // Set human power to zero to avoid a jump when restarting
+  Throttle_Value = NeutralThrottleValue;    // Reset throttle to neutral value
+}
+
+void ResetTime() {
+  NextLoopTime = micros() + LoopTimeUs;     // Reset loop time to avoid a long wait before the next loop
+}
+
+void StopBike() // Function to stop the bike by resetting the state and applying the neutral throttle value
+{
+  analogWrite(ThrottlePin, NeutralThrottleValue); // Apply the throttle value to stop the bike
+  ResetBike();
+  ResetTime();
 }
